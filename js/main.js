8c8a80be625c86ca856078d2012e3b79115b6de6
jiangcs520 <478283225@qq.com>
2014-03-27 23:29:38 +0800

    add and modify

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+    var winH = $(window).height();
+    $("#introduce").css({
+        "margin-top": winH - $(".resume").outerHeight()-150
+    });
+    $(window).bind("scroll", scrollHandler);
+});
+
+function scrollHandler(event) {
+    console.log($(this).scrollTop());
+}
