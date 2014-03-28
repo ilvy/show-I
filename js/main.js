02aead1135ae67f4f53de903a07ebd5c5c67c081
jiangcs520 <478283225@qq.com>
2014-03-28 18:37:51 +0800

    add

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
 ﻿$(function() {
     var winH = $(window).height();
-    $("#introduce").css({
-        "margin-top": winH - $(".resume").outerHeight()-150
+    var $resume = $(".resume");
+    $(".resume").css({
+        "margin-top":(winH - $resume.height())/2
     });
+    $("#introduce").css({
+        "margin-top": winH - $(".resume").outerHeight() - $(".resume").offset().top - 50
+    });
+    //$("#categories").
     $(window).bind("scroll", scrollHandler);
 });
 
+var $barOriginTop,$barOriginWid,$barOriginHeight;
 function scrollHandler(event) {
     console.log($(this).scrollTop());
-    $(".resume,#introduce").each(function() {
-        var $section = $(this);
-        var top = $section.offset().top;
-        $section.css({
-            marginTop:top-100
-        })
-    });
+    var wheelScrolltop = $(this).scrollTop();
+    var $bar = $("#fixed-bar");
+    $barOriginWid = $bar.width();
+    $barOriginHeight = $bar.height();
+    if ($bar.offset().top <= wheelScrolltop && $bar.css("position") != "fixed") {
+        $barOriginTop = $bar.offset().top;
+        $bar.css({
+            "position": "fixed",
+            "top": 0,
+            width:$barOriginWid,
+            height: $barOriginHeight
+        });
+        $("#categories").css({
+            position: "absolute",
+            top:$bar.height()
+        });
+    } else if ($barOriginTop > wheelScrolltop && $bar.css("position") == "fixed") {
+        $bar.css({
+            position: "static"
+        });
+        $("#categories").css({
+            position: "static",
+            "margin-top": $bar.height()
+        });
+    }
 }
