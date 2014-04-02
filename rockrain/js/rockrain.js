ace4cd9ed7647dd067326df46b11d4dc672891c7
jiangcs520 <478283225@qq.com>
2014-04-02 08:37:20 +0800

    game

---
+++ rockrain/js/rockrain.js
@@ -0,0 +1,45 @@
+﻿(function(window) {
+    var ctx,winH,winW;
+    function Rockrain(canvas,context) {
+        ctx = context;
+        winH = canvas.height;
+        winW = canvas.width;
+        this.init();
+    }
+
+    var _rr;
+    Rockrain.prototype = {
+        init: function () {
+            _rr = this;
+            setInterval(function() {
+                _rr.run();
+            },50);
+        },
+        /**
+         *@desc:执行帧
+        */
+        run:function() {
+            this.setBg();
+            this.updateMan();
+            this.updateRocks();
+            this.updateBonus();
+        },
+        setBg: function () {
+            ctx.beginPath();
+            ctx.fillStyle = "#ffffff";
+            ctx.fillRect(0, 0, winW, winH);
+            ctx.closePath();
+            ctx.stroke();
+        },
+        updateMan:function() {
+            
+        },
+        updateRocks:function() {
+            
+        },
+        updateBonus:function() {
+            
+        }
+    };
+    window.Rockrain = Rockrain;
+})(window);
---
+++ rockrain/js/util.js
@@ -0,0 +1,13 @@
+﻿(function() {
+    var utils = {
+        /*
+         *@desc:创建图片
+         */
+        createImg:function(imgPath) {
+            var img = new Image();
+            img.src = imgPath;
+            return img;
+        }
+    };
+    window.utils = utils;
+})();
